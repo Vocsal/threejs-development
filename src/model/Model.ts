a72c2a5a121c6ad994be366bfcea45f84741f2a6
张英贵 <zhangyinggui@wondershare.cn>
2021-09-07 20:30:51 +0800

    modify Model class

--- src/model/Model.ts
+++ src/model/Model.ts
@@ -1,15 +1,6 @@
 import * as THREE from "three";
 
-export default class Model {
-    Object3D!: THREE.Object3D;
-    oldScale: number = 1;
-    scale: number = 1;
-
-    setScale(scale: number): void {
-        this.oldScale = this.scale;
-        this.scale = scale;
-        this.updateApplyProperty();
-    };
+export default class Model extends THREE.Group {
 
     updateApplyProperty(): void {
         // 模型属性改变后，进行适应
@@ -27,8 +18,7 @@ export default class Model {
         rotationY: number,
         rotationZ: number,
     ) {
-        const scale = this.scale;
-        const boxGeometry = new THREE.BoxGeometry(width * scale, height * scale, depth * scale);
+        const boxGeometry = new THREE.BoxGeometry(width, height, depth);
         const cube = new THREE.Mesh(boxGeometry, material);
         cube.position.set(positionX, positionY, positionZ);
         cube.rotation.set(rotationX, rotationY, rotationZ);
